8459818bceb47822832c9939dd6df00a97e41c4a
sCherry1101 <cherry.1101.8113@gmail.com>
2026-09-16 19:26:13 +0530

    add : controls.svelte( the nav panel)

--- src/routes/science/mechanical-advantages/+page.svelte
+++ src/routes/science/mechanical-advantages/+page.svelte
@@ -1,4 +1,7 @@
 <script>
-import './styling.css'
-import Tabs from '$component/Tabs'
-import 
+    import './styling.css'
+    import Tabs from '$component/Tabs'
+    import Lever from 'lever.svelte'
+    import Formula from 'formula.svelte'
+    import Controls from 'controls.svelte'
+
---
+++ src/routes/science/mechanical-advantages/controls.svelte
@@ -0,0 +1,23 @@
+<script>
+  let { load, effortArm, loadArm } = $props()
+</script>
+
+<div class="controls">
+  <label>
+    Load
+    <input type="range" min="10" max="200" step="10" bind:value={load} />
+    <span>{load} N</span>
+  </label>
+
+  <label>
+    Effort arm
+    <input type="range" min="0.2" max="2" step="0.1" bind:value={effortArm} />
+    <span>{effortArm} m</span>
+  </label>
+
+  <label>
+    Load arm
+    <input type="range" min="0.1" max="1.5" step="0.1" bind:value={loadArm} />
+    <span>{loadArm} m</span>
+  </label>
+</div>
