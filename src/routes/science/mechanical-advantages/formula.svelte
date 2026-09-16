846762eec6be71f34b068bb03f0ed6812aa69730
sCherry1101 <cherry.1101.8113@gmail.com>
2026-09-16 19:27:35 +0530

    add: formula.svelte

---
+++ src/routes/science/mechanical-advantages/formula.svelte
@@ -0,0 +1,12 @@
+<script>
+  let { load, effort, mechanicalAdvantage } = $props()
+</script>
+
+<div class="formula">
+  <h2>Mechanical Advantage</h2>
+  <p>MA=Load ÷ Effort</p>
+  <p>
+    MA={load} ÷ {effort.toFixed(1)}
+  </p>
+  <strong>MA={mechanicalAdvantage.toFixed(2)}×</strong>
+</div>
